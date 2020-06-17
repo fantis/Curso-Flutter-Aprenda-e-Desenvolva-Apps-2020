f1431272bd2cac2a21c6afaa52466b1b5495940b
Rubens Pereira <rubenscp@gmail.com>
2020-06-17 19:23:01 -0300

    Aula 224

--- projects/shop/lib/providers/cart.dart
+++ projects/shop/lib/providers/cart.dart
@@ -5,12 +5,14 @@ import 'package:shop/providers/product.dart';
 
 class CartItem {
   final String id;
+  final String productId;
   final String title;
   final int quantity;
   final double price;
 
   CartItem({
     @required this.id,
+    @required this.productId,
     @required this.title,
     @required this.quantity,
     @required this.price,
@@ -42,6 +44,7 @@ class Cart with ChangeNotifier {
         product.id,
         (existItem) => CartItem(
           id: existItem.id,
+          productId: existItem.productId,
           title: existItem.title,
           quantity: existItem.quantity + 1,
           price: existItem.price,
@@ -52,12 +55,20 @@ class Cart with ChangeNotifier {
         product.id,
         () => CartItem(
           id: Random.secure().nextDouble().toString(),
+          productId: product.id,
           title: product.title,
           quantity: 1,
           price: product.price,
         ),
       );
     }
+
+    // notifying listeners related
+    notifyListeners();
+  }
+
+  void removeItem(String productId) {
+    this._items.remove(productId);
 
     // notifying listeners related
     notifyListeners();
